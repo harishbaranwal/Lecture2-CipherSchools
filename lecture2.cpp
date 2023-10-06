f4f5d348b640675bfa36b385b32152e04a66651b
harishbaranwal <114431658+harishbaranwal@users.noreply.github.com>
2023-10-06 23:36:52 +0530

    Create lecture2.cpp

---
+++ lecture2.cpp
@@ -0,0 +1,11 @@
+ #include <iostream>
+ // this is a header.
+ using namespace std;
+
+ int main(){
+
+    cout << "Hello world"; // this will print Hello world on user screen.
+
+    return 0;
+    // this text in green color are comments.
+ }
